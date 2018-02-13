bc2774afd14bd953a72e3adaa1049a7e97d88ca1
Raysa Dutra <rai.dietra4@yahoo.com>
2018-02-13 16:17:17 -0200

    add: minos sounds

---
+++ labyrinth-hunter/Assets/Scripts/AnimationMinos.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AnimationStates{
+	WALK,
+	RUN,
+	IDDLE,
+    SHOUT,
+    ATTACK,
+}
+
+public class AnimationMinos : MonoBehaviour {
+
+	public Animator animator;
+    private AudioScript audioMino = new AudioScript();
+
+    public void PlayAnimation (AnimationStates statesAnimation) {
+
+		switch (statesAnimation) {
+		    case AnimationStates.IDDLE:
+			    {
+				    StopAnimations ();
+				    animator.SetBool ("inIdle", true);
+			    }
+			    break;
+		    case AnimationStates.WALK:
+			    {
+				    StopAnimations ();
+				    animator.SetBool ("inWalk", true);
+			    }
+			    break;
+		    case AnimationStates.RUN:
+			    {
+				    StopAnimations ();
+				    animator.SetBool ("inRun", true);
+			    }
+			    break;
+            case AnimationStates.SHOUT:
+			    {
+                    StopAnimations();
+                    animator.SetBool("inShout", true);
+                    audioMino.PlayMinoSFX4();
+                }
+                break;
+            case AnimationStates.ATTACK:
+			    {
+                    StopAnimations();
+                    animator.SetBool("inAttack", true);
+                }
+			    break;
+		}
+    }
+
+	void StopAnimations(){
+		animator.SetBool ("inRun", false);
+		animator.SetBool ("inWalk", false);
+		animator.SetBool ("inIdle", false);
+        animator.SetBool("inShout", false);
+        animator.SetBool("inAttack", false);
+    }
+
+}﻿
---
+++ labyrinth-hunter/Assets/Scripts/StartMinosMoviment.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMinosMoviment : MonoBehaviour {
+
+    private AnimationMinos animaMinos = new AnimationMinos();
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
