738ccbeb96fb0b02df5cca0288d3a3216e8f8260
Herik Tedesco <herik.tedesco@al.infnet.edu.br>
2018-02-13 01:39:22 -0200

    update: update animation

--- labyrinth-hunter/Assets/Scripts/chaseMinotaur.cs
+++ labyrinth-hunter/Assets/Scripts/chaseMinotaur.cs
@@ -1,50 +1,47 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AnimationStates{
+	WALK,
+	RUN,
+	IDDLE,
+}
 
 public class chaseMinotaur : MonoBehaviour {
 
-	public Transform player;
-	static Animator anim;
+	public Animator animator;
 
-	// Use this for initialization
-	void Start () 
-	{
-		anim = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		Vector3 direction = player.position - this.transform.position;
-		float angle = Vector3.Angle(direction,this.transform.forward);
-		if(Vector3.Distance(player.position, this.transform.position) < 10 && angle < 30)
-		{
-			
-			direction.y = 0;
+	public void PlayAnimation (AnimationStates statesAnimation) {
 
-			this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
-										Quaternion.LookRotation(direction), 0.1f);
-
-			anim.SetBool("isIdle",false);
-			if(direction.magnitude > 5)
+		switch (statesAnimation) {
+		case AnimationStates.IDDLE:
 			{
-				this.transform.Translate(0,0,0.05f);
-				anim.SetBool("isWalking",true);
-				anim.SetBool("isAttacking",false);
+				StopAnimations ();
+				animator.SetBool ("inIdle", true);
 			}
-			else
+			break;
+		case AnimationStates.WALK:
 			{
-				anim.SetBool("isAttacking",true);
-				anim.SetBool("isWalking",false);
+				StopAnimations ();
+				animator.SetBool ("inWalk", true);
 			}
-
-		}
-		else 
-		{
-			anim.SetBool("isIdle", true);
-			anim.SetBool("isWalking", false);
-			anim.SetBool("isAttacking", false);
+			break;
+		case AnimationStates.RUN:
+			{
+				StopAnimations ();
+				animator.SetBool ("inRun", true);
+			}
+			break;
 		}
 
+
 	}
-}
+
+	void StopAnimations(){
+		animator.SetBool ("inRun", false);
+		animator.SetBool ("inWalk", false);
+		animator.SetBool ("inIdle", false);
+	}
+
+}﻿
